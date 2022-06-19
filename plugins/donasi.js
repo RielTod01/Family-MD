7bf940f32c776930fc41e6db90a6dc868c683ea7
RielTod01 <107606413+RielTod01@users.noreply.github.com>
2022-06-20 05:53:50 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,13 +2,13 @@ const qrku = "https://i.ibb.co/MnbqZ0N/qr-Aguz-Familia-07-06-22-1654614928.png"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [081320170984]
-│ • Dana  [081320170984]
+│ • 𝐀𝐗𝐈𝐒 [083813417529]
+│ • 𝐃𝐀𝐍𝐀  [083813417529]
 ╰────
 ╭─「 *NOTE* 」
 │ > Ingin donasi? Wa.me/6281320170984
 │ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _atau beli *RDP/VPS/PANEL* agar bot bisa jalan_
 │ _24jam tanpa kendala_
 ╰────
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
